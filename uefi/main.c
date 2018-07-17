e0a285408736479d79a35c6a723d4df7918d7378
Ciro Santilli <ciro.santilli@gmail.com>
2018-07-17 09:46:13 +0100

    link to examples from readme
    Do another cleanup round while I'm at it.

--- uefi/main.c
+++ uefi/main.c
@@ -8,16 +8,3 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
    Print(L"Hello, world!\n");
    return EFI_SUCCESS;
 }
-
-/*
-#include <efi.h>
-#include <efilib.h>
-
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-  SIMPLE_TEXT_OUTPUT_INTERFACE *conout;
-  InitializeLib(ImageHandle, SystemTable);
-  conout = SystemTable->ConOut;
-  uefi_call_wrapper(conout->OutputString, 2, conout, (CHAR16 *)L"Hello World\n\r");
-  return EFI_SUCCESS;
-}
-*/

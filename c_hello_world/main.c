589240ebd1c060a36916682b6ad4f8b2bedd634f
Ciro Santilli <ciro.santilli@gmail.com>
2018-12-03 00:39:26 +0000

    c hello world: ok, I dont know how to debug this further

--- c_hello_world/main.c
+++ c_hello_world/main.c
@@ -1,7 +1,10 @@
 void main(void) {
     int i;
     char s[] = "hello world";
+    /* Add a bunch of debug prints to see if the control loop is correct. It is. */
+    __asm__ ("mov $0x0E40, %ax; int $0x10");
     for (i = 0; i < sizeof(s); ++i) {
+        __asm__ ("mov $0x0E41, %ax; int $0x10");
         __asm__ (
             "mov %0, %%ax; int $0x10"
             :
@@ -9,6 +12,7 @@ void main(void) {
             : "%ax"
         );
     }
+    __asm__ ("mov $0x0E40, %ax; int $0x10");
     while (1) {
         __asm__ ("hlt");
     };
